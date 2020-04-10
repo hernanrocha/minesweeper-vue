bc217f0bbac981f9e117e54d5c8ce90c3149024c
Hernan Rocha <hernanrocha93@gmail.com>
2020-04-10 12:31:35 -0300

    First game frontend version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,47 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    <h1>Minesweeper Game</h1>
+
+
+    <div class="game-container">
+      <table id="game">
+        <tr v-for="(row, i) in game.board" :key="i">
+          <td v-for="(cell, j) in row" :key="j" @click.left="reveal(i, j)" @click.right.prevent="flag(i, j)">
+            <span v-if="cell === 0"></span>
+            <span v-else-if="cell === -1"></span>
+            <span v-else-if="cell === -2">F</span>
+            <span v-else-if="cell === -3">-</span>
+            <span v-else>{{cell}}</span>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Rows: {{game.rows}}</li>
+      <li class="list-group-item">Cols: {{game.cols}}</li>
+      <li class="list-group-item">Mines: {{game.mines}}</li>
+      <li class="list-group-item">Started At: {{game.created_at | timestamp }}</li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <button type="button" class="btn btn-primary" @click="newGame">New Game</button>
   </div>
 </template>
+
+<style scoped lang="scss">
+.game-container {
+  margin: 20px 0;
+}
+#game {
+  margin: auto;
+}
+
+#game td {
+  border: solid 1px;
+  width: 50px;
+  height: 50px;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -42,23 +51,75 @@ export default Vue.extend({
   props: {
     msg: String,
   },
+  data () {
+    return { 
+      game: {
+        "rows": 5,
+        "cols": 5,
+        "mines": 3,
+        "board": [
+            [
+                -1, 0, 0, 0, 0
+            ],
+            [
+                0, 4,-2, 0, 0
+            ],
+            [
+                0,-1, 0, 0, 0
+            ],
+            [
+                0,0,0,0,0
+            ],
+            [
+                0, 0, 0, 0,-1
+            ]
+        ],
+        "created_at": "2020-04-10T11:22:42.655249633-03:00"
+      }
+    }
+  },
+  methods: {
+    reveal(i, j) {
+      console.log('Reveal: ', i, j);
+      const gameId = 1;
+      const cell = { row: i, col: j }
+      this.$http
+        .post(`/api/v1/game/${gameId}/reveal`, cell)
+        .then(response => {
+          console.log({ response });
+          this.game = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+      });
+    },
+    flag(i, j) {
+      console.log('Flag: ', i, j);
+      const gameId = 1;
+      const cell = { row: i, col: j }
+      this.$http
+        .post(`/api/v1/game/${gameId}/flag`, cell)
+        .then(response => {
+          console.log({ response });
+          this.game = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+      });
+    },
+    newGame() {
+      console.log('New Game');
+      const gameConfig = {}
+      this.$http
+        .post("/api/v1/game", gameConfig)
+        .then(response => {
+          console.log({ response });
+          this.game = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+      });
+    }
+  }
 });
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import Vue from 'vue'
+import axios from 'axios';
+import "bootstrap";
+import moment from 'moment';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "material-design-icons-iconfont/dist/material-design-icons.css";
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.filter("timestamp", value => {
+  return moment(value).format("LLL");
+});
+
+// AXIOS
+Vue.prototype.$http = axios;
+axios.defaults.baseURL = "http://localhost:8002";
+// axios.defaults.baseURL = process.env.VUE_APP_ROOT_API;
 
 new Vue({
   router,
