ceac1019255daffca1d3015c7c65342e2cf625a3
Hernan Rocha <hernanrocha93@gmail.com>
2020-04-10 18:23:09 -0300

    Fix game layout

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <h1>Minesweeper Game</h1>
+    <h1 class="title">Minesweeper Game</h1>
 
     <div class="container">
       <div class="row">
-        <div class="col-md game-container">
-          <table id="game">
-            <tr v-for="(row, i) in game.board" :key="i">
-              <td v-for="(cell, j) in row" :key="j" @click.left="reveal(i, j)" @click.right.prevent="flag(i, j)">
-                <span v-if="cell === 0"></span>
-                <span v-else-if="cell === -1"></span>
-                <span v-else-if="cell === -2">F</span>
-                <span v-else-if="cell === -3">-</span>
-                <span v-else-if="cell === -5">X</span>
-                <span v-else>{{cell}}</span>
-              </td>
-            </tr>
-          </table>
+        <div class="col-md-9">
+          <div v-if="game.board">
+            <table  id="game">
+              <tr v-for="(row, i) in game.board" :key="i">
+                <td v-for="(cell, j) in row" :key="j" @click.left="reveal(i, j)" @click.right.prevent="flag(i, j)" v-bind:class="{ small: game.rows > 8 || game.cols > 8 }">
+                  <span v-if="cell === 0"></span>
+                  <span v-else-if="cell === -1"></span>
+                  <span v-else-if="cell === -2" class="flag">F</span>
+                  <span v-else-if="cell === -3">-</span>
+                  <span v-else-if="cell === -5" class="bomb">X</span>
+                  <span v-else>{{cell}}</span>
+                </td>
+              </tr>
+            </table>
+            <p class="game-info">Game ID: <b>{{game.id}}</b> - Playing Time: {{ elapsedTime }}</p>
+          </div>
+          <div v-else><b>Please Create a New Game</b></div>
         </div>
 
-        <div class="col-md text-right">
+        <div class="col-md-3 text-right">
           <form @submit="newGame">
             <div class="form-group row">
               <label for="rows" class="col-sm-4 col-form-label">Rows</label>
@@ -67,33 +71,62 @@
 
         </div>
       </div>
-    </div>
 
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Game ID: {{game.id}}</li>
-        <li class="list-group-item">Status: {{game.status}}</li>
-        <li class="list-group-item">Started At: {{game.created_at | timestamp }}</li>
-      </ul>
+      <div class="row game-alert">
+        <div class="col-sm">
+          <div v-if="game.status == 'won'" class="alert alert-success" role="alert">
+            Congratulations! You have won
+          </div>
+          <div v-else-if="game.status == 'lose'" class="alert alert-danger" role="alert">
+            Oops! You have selected a bomb. Try again!
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.game-container {
-  margin: 20px 0;
+h1.title {
+  margin-bottom: 30px;
 }
+
 #game {
   margin: auto;
 }
 
 #game td {
+  font-size: 18px;
   border: solid 1px;
   width: 50px;
   height: 50px;
+}
+
+#game td.small {
+  width: 35px;
+  height: 35px;
+}
+
+.game-alert {
+  margin-top: 10px;
+}
+
+.game-info {
+  font-size: 12px;
+}
+
+.bomb {
+  color: red;
+}
+
+.flag {
+  color: green;
 }
 </style>
 
 <script lang="ts">
 import Vue from 'vue';
+import moment from 'moment';
 
 export default Vue.extend({
   name: 'GameBoard',
@@ -103,43 +136,29 @@ export default Vue.extend({
   data () {
     return {
       form: {
-        rows: 6,
-        cols: 6,
-        mines: 5
+        rows: 10,
+        cols: 10,
+        mines: 12
       },
-      game: {
-        "rows": 5,
-        "cols": 5,
-        "mines": 3,
-        "board": [
-            [
-                -1, 0, 0, 0, 0
-            ],
-            [
-                0, 4,-2, 0, 0
-            ],
-            [
-                0,-1, 0, 0, 0
-            ],
-            [
-                0,0,0,0,0
-            ],
-            [
-                0, 0, 0, 0,-1
-            ]
-        ],
-        "created_at": "2020-04-10T11:22:42.655249633-03:00"
-      }
+      game: {},
+      now: new Date().toString()
     }
+  },
+  mounted() {
+    this.newGame();
+    setInterval(() => {
+      if (!this.game || !this.game.created_at || this.game.status !== 'playing') {
+        return;
+      }
+      this.now = new Date();
+    }, 1000)
   },
   methods: {
     reveal(i, j) {
-      console.log('Reveal: ', i, j);
       const cell = { row: i, col: j }
       this.$http
         .post(`/api/v1/game/${this.game.id}/reveal`, cell)
         .then(response => {
-          console.log({ response });
           this.game = response.data;
         })
         .catch(error => {
@@ -147,12 +166,10 @@ export default Vue.extend({
       });
     },
     flag(i, j) {
-      console.log('Flag: ', i, j);
       const cell = { row: i, col: j }
       this.$http
         .post(`/api/v1/game/${this.game.id}/flag`, cell)
         .then(response => {
-          console.log({ response });
           this.game = response.data;
         })
         .catch(error => {
@@ -160,7 +177,6 @@ export default Vue.extend({
       });
     },
     newGame() {
-      console.log('New Game');
       const gameConfig = {
         rows: parseInt(this.form.rows),
         cols: parseInt(this.form.cols),
@@ -169,7 +185,6 @@ export default Vue.extend({
       this.$http
         .post("/api/v1/game", gameConfig)
         .then(response => {
-          console.log({ response });
           this.game = response.data;
         })
         .catch(error => {
@@ -177,7 +192,6 @@ export default Vue.extend({
       });
     },
     loadGame() {
-      console.log('Load Game');
       this.$http
         .get(`/api/v1/game/${this.form.gameId}`)
         .then(response => {
@@ -186,7 +200,44 @@ export default Vue.extend({
         })
         .catch(error => {
           console.error(error);
+          alert('Game not found');
       });
+    }
+  },
+  computed: {
+    elapsedTime() {
+      if (!this.game || !this.game.created_at) {
+        return '';
+      }
+
+      const now = moment(this.now);
+      const before = moment(this.game.created_at);
+
+      let seconds = Math.floor((now - before) / 1000);
+      if (seconds < 0) seconds = 0;
+
+      let minutes = Math.floor(seconds / 60);
+      seconds = seconds % 60;
+
+      if (minutes == 0) {
+        return `${seconds}s`;
+      }
+
+      let hours = Math.floor(minutes / 60);
+      minutes = minutes % 60;
+
+      if (hours == 0) {
+        return `${minutes}m ${seconds}s`;
+      }
+
+      const days = Math.floor(hours / 24);
+      hours = hours % 24;
+
+      if (days == 0) {
+        return `${hours}h ${minutes}m ${seconds}s`;
+      }
+
+      return `${days} days ${hours}h ${minutes}m ${seconds}s`;
     }
   }
 });
